f74da901d8abb73e7796521a5eaf3fb217f91efc
Dorian <pro.dorianlopez@gmail.com>
2025-07-21 15:55:57 +0200

    Modification des transitions d'animation dans le composant Home : changement du type de transition de 'ease' à 'spring' pour améliorer l'effet visuel.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -56,7 +56,7 @@ export default function Home() {
     visible: {
       opacity: 1,
       y: 0,
-      transition: { type: 'ease', duration: 0.8 },
+      transition: { type: 'spring', duration: 0.8 },
     },
   };
   const bottomElementVariants = {
@@ -64,7 +64,7 @@ export default function Home() {
     visible: {
       opacity: 1,
       y: 0,
-      transition: { type: 'ease', duration: 0.8, delay: 0.2 },
+      transition: { type: 'spring', duration: 0.8, delay: 0.2 },
     },
   };
 
