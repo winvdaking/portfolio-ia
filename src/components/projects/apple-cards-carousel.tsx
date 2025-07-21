4add55b872768fcb481c362524b358b2cabd58a3
Dorian <pro.dorianlopez@gmail.com>
2025-07-21 16:15:26 +0200

    Correction de la fonction onCardClose dans le contexte du carrousel et amélioration de la structure des transitions d'animation dans le composant Carousel, en déplaçant les options de transition dans la bonne propriété.

--- src/components/projects/apple-cards-carousel.tsx
+++ src/components/projects/apple-cards-carousel.tsx
@@ -27,7 +27,7 @@ export const CarouselContext = createContext<{
   onCardClose: (index: number) => void;
   currentIndex: number;
 }>({
-  onCardClose: () => {},
+  onCardClose: () => { },
   currentIndex: 0,
 });
 
@@ -133,16 +133,16 @@ export const Carousel = ({
                   opacity: 0,
                   y: 20,
                 }}
-                animate={{
+                whileInView={{
                   opacity: 1,
                   y: 0,
-                  transition: {
-                    duration: 0.5,
-                    delay: 0.2 * index,
-                    ease: 'easeOut',
-                    once: true,
-                  },
                 }}
+                transition={{
+                  duration: 0.5,
+                  delay: 0.2 * index,
+                  ease: 'easeOut',
+                }}
+                viewport={{ once: true }}
                 key={'card' + index}
                 className="rounded-3xl last:pr-[5%] md:last:pr-[33%]"
               >
